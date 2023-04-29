a0461739ee48c63117398293fda737171a096621
RashadMusayev23 <rashadm@crocusoft.com>
2023-04-29 23:51:28 +0400

    Initial Commit

---
+++ app/src/main/java/com/mazeppa/firebasechat/registration/OTPActivity.kt
@@ -0,0 +1,12 @@
+package com.mazeppa.firebasechat.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mazeppa.firebasechat.R
+
+class OTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otpactivity)
+    }
+}
---
+++ app/src/main/java/com/mazeppa/firebasechat/registration/VerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.mazeppa.firebasechat.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mazeppa.firebasechat.R
+
+class VerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+    }
+}
